f0f54f9654b2052a3c9d2074c41a073ea4184db6
MrAkbari <dhruvakbari303@gmail.com>
2023-05-04 22:45:42 +0530

    connect mongoose and hello word

---
+++ Backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI).then(() => console.log('Connected Successfully')).catch(error => console.log(error));
+}
+
+
+
+module.exports = connectToMongo;
